1c36a93e4eab9dc719394d8f9b5ef7d161aef5c2
Fernando Nuñez <fernandojne.20@gmail.com>
2019-05-28 00:31:17 -0500

    was added the functionality of the routing for the navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/contacts'},
+  {path: 'contacts', loadChildren: './contacts/contacts.module#ContactsModule'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/contacts/contacts-routing.module.ts
+++ src/app/contacts/contacts-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DirectoryComponent } from './directory/directory.component';
+import { TempDirectoryComponent } from './temp-directory/temp-directory.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { ContactsComponent } from './contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ContactsComponent,
+  children: [
+    {path: '', pathMatch: 'prefix', redirectTo: 'directory'},
+    {path: 'directory', component: DirectoryComponent},
+    {path: 'tempDirectory', component: TempDirectoryComponent}
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
